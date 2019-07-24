8c7788f346e3424ede5c7018c9037989be4081c9
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-07-24 10:13:10 +0530

    Smart refrigirator solution

---
+++ SmartRefridgerator/Refrigerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SmartRefridgerator
+{
+    public class Refrigerator
+    {
+        private VegetableTray _vegetableTray = new VegetableTray();
+
+        private ConfigurationManager _configurationManager = new ConfigurationManager(new InMemoryStorage());
+        
+        public void AddVegetable(Vegetable vegetable, int quantity)
+        {
+            _vegetableTray.Add(vegetable, quantity);
+        }
+
+        public void TakeOutVegetable(Vegetable vegetable, int quantity)
+        {
+            _vegetableTray.TakeOut(vegetable, quantity);
+
+            var vegetableQuantity = _vegetableTray.GetVegetableQuantity();
+        }
+
+        //Testing code
+        public List<KeyValuePair<Vegetable, int>> GetVegetableQuantity()
+        {
+            return _vegetableTray.GetVegetableQuantity();
+        }
+
+
+    }
+}
---
+++ SmartRefridgerator/VegetableTracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace SmartRefridgerator
+{
+    public class VegetableTracker
+    {
+        Refrigerator refrigerator = new Refrigerator();
+        ConfigurationManager configurationManager;
+        public VegetableTracker(Refrigerator refrigerator, ConfigurationManager configurationManager)
+        {
+            this.refrigerator = refrigerator;
+            this.configurationManager = configurationManager;
+        }
+        public List<KeyValuePair<Vegetable, int>> GetInsufficientVegetableQuantity()
+        {
+            var insufficientVegetableQuantityList = new List<KeyValuePair<Vegetable, int>>();
+            //var vegetableTray = new VegetableTray();
+            var vegetablesList = refrigerator.GetVegetableQuantity();
+            //var inMemoryStorage = new InMemoryStorage();
+            foreach(var vegetable in vegetablesList)
+            {
+               var vegetableMinimumQuantity = configurationManager.GetMinimumQuantity(vegetable.Key);
+                if (vegetableMinimumQuantity >= vegetable.Value)
+                    insufficientVegetableQuantityList.Add(vegetable);
+            }
+            
+            return insufficientVegetableQuantityList;
+        }
+    }
+}
